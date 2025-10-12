eb6649eed92c1dcfc215e7ab5e77b5d7be59fc37
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-10-12 17:03:35 +0100

    integrating payment verification and flutter webhoon integration

--- src/routes/transactionRoute.js
+++ src/routes/transactionRoute.js
@@ -18,14 +18,27 @@ router.post('/initialize', async (req, res) => {
   }
 });
 
-router.post('/verify', async (req, res) => {
+// router.post('/verify', async (req, res) => {
+//   try {
+//     await verifyPayment(req.body);
+//     res.status(200).send('Webhook received');
+//   } 
+//   catch (err) {
+//     console.error(err);
+//     res.status(400).send(err.message);
+//   }
+// });
+
+router.post("/verify", async (req, res) => {
   try {
-    await verifyPayment(req.body);
-    res.status(200).send('Webhook received');
+    console.log("Webhook received:", req.body);
+    const result = await verifyPayment(req, process.env.FLW_SECRET_HASH);
+    res.status(200).json(result);
+
   } 
   catch (err) {
-    console.error(err);
-    res.status(400).send(err.message);
+    console.error("Webhook error:", err.message);
+    res.status(400).json({ success: false, message: err.message });
   }
 });
 
