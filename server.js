6afa32adffe2b55110b72280555cfa0642bb5f5d
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-10-21 11:03:45 +0100

    implemented transaction history

--- server.js
+++ server.js
@@ -207,12 +207,16 @@ import dotenv from 'dotenv';
 import userRoutes from './src/routes/userRoute.js';
 import transactionRoutes from './src/routes/transactionRoute.js';
 import adminRoutes from './src/routes/adminRoutes.js';
+import cors from "cors";
+
 
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cors({ origin: "*"}));
+
 
 app.use('/api/users', userRoutes);
 app.use('/api/payment', transactionRoutes);
