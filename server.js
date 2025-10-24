0a665628c4329c0118a73911497b1e0d19c13f9b
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-10-24 13:28:07 +0100

    refactoring items and assigning every user endpoint to their routes

--- server.js
+++ server.js
@@ -207,7 +207,7 @@ import dotenv from 'dotenv';
 import userRoutes from './src/routes/userRoute.js';
 import transactionRoutes from './src/routes/transactionRoute.js';
 import adminRoutes from './src/routes/adminRoutes.js';
-import itemRoutes from './src/routes/itemRoutes.js';
+// import itemRoutes from './src/routes/itemRoutes.js';
 import cors from "cors";
 
 
@@ -221,7 +221,6 @@ app.use(cors({ origin: "*"}));
 app.use('/api/users', userRoutes);
 app.use('/api/payment', transactionRoutes);
 app.use('/api/admin', adminRoutes);
-app.use('/api/items', itemRoutes);
 
 
 const PORT = process.env.PORT || 5000;
