57d6e44425a6a00b38ed378362d7d2888d4f36f8
Victor <victorbondzie49@gmail.com>
2025-10-26 09:51:11 +0000

    New changes

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,17 +1,38 @@
 import jwt from "jsonwebtoken";
 
+// export const verifyToken = (req, res, next) => {
+//   const token = req.headers.authorization?.split(" ")[1];
+//   if (!token) return res.status(401).json({ message: "Access denied" });
+
+//   try {
+//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     req.user = decoded;
+//     next();
+//   } catch (err) {
+//     res.status(403).json({ message: "Invalid token" });
+//   }
+// };
+
+
 export const verifyToken = (req, res, next) => {
-  const token = req.headers.authorization?.split(" ")[1];
-  if (!token) return res.status(401).json({ message: "Access denied" });
+  // header: "Authorization: Bearer <token>"
+  const headerToken = req.headers?.authorization?.split(' ')[1];
+  const cookieToken = req.cookies?.authToken;
+  const token = headerToken || cookieToken;
+  if (!token) return res.status(401).json({ message: 'Access denied. No token provided.' });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    res.status(403).json({ message: "Invalid token" });
+    return res.status(403).json({ message: 'Invalid or expired token.' });
   }
 };
+
+
+
+
 
 export const verifyAdmin = (req, res, next) => {
   // console.log("Decoded token user:", req.user);
