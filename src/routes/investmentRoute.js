80215226415f76d85231489fbd804cd773390d4f
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-11-06 08:49:29 +0100

    Updating investment route and investment service

--- src/routes/investmentRoute.js
+++ src/routes/investmentRoute.js
@@ -1,13 +1,19 @@
+import express from 'express';
 import { createInvestment } from '../services/investmentService.js';
+import { verifyToken } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-export const createInvestmentController = async (req, res) => {
+router.post('/createInvestment', verifyToken, async (req, res) => {
   try {
-    const { userId, itemId } = req.body;
+    const userId = req.user.id;
+    const { itemId } = req.body;
     const investment = await createInvestment(userId, itemId);
     res.status(201).json({ success: true, data: investment });
-  } catch (error) {
+  } 
+  catch (error) {
     res.status(400).json({ success: false, message: error.message });
   }
-};
+});
+
+export default router;
