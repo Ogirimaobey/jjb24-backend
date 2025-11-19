c7baa9241276d99717b4cc4a471aa5d31fba8aa8
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-11-19 10:43:55 +0100

    Implementinng user profile endpoint to fetch logged in user's data from databae

--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -112,6 +112,8 @@ router.get('/user_profile', verifyToken, async (req, res) => {
   }
 });
 
+
+
 // Get user earnings summary (today, yesterday, total)
 router.get('/earnings-summary', verifyToken, async (req, res) => {
   try {
