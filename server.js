3fad265fdf2862a47171ec74825f943fbe43451f
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-11-21 15:03:59 +0100

    update

--- server.js
+++ server.js
@@ -50,6 +50,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 // Runs every 24 hours
-cron.schedule('* * * * *', async () => {
+cron.schedule('0 0 * * *', async () => {
   await processDailyEarnings();
 });
