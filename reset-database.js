d00a3de048330cfbed45c8bbd2d39207aa45cfb6
BabatundeOla1 <Olaleyebabatun@gmail.com>
2025-11-27 08:39:51 +0100

    update

--- reset-database.js
+++ reset-database.js
@@ -1,22 +1,3 @@
-// import pool from "./src/config/database.js";
-
-// const resetTables = async () => {
-//   try {
-//     await pool.query("TRUNCATE TABLE transactions RESTART IDENTITY CASCADE;");
-//     await pool.query("TRUNCATE TABLE users RESTART IDENTITY CASCADE;");
-//     await pool.query("TRUNCATE TABLE daily_tasks RESTART IDENTITY CASCADE;");
-//     await pool.query("TRUNCATE TABLE investments RESTART IDENTITY CASCADE;");
-//     console.log("All tables has been cleared successfully.");
-//     process.exit(0);
-//   } catch (error) {
-//     console.error("Error clearing tables:", error.message);
-//     process.exit(1);
-//   }
-// };
-
-// resetTables();
-
-
 import pool from "./src/config/database.js";
 
 const resetTables = async () => {
