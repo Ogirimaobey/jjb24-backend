96ff65b9b77a0829562eca24e358b5cbac2414b9
Ogirimaobey <ogirimaobey@gmail.com>
2025-12-13 19:44:28 +0100

    Update itemRepository.js

--- src/repositories/itemRepository.js
+++ src/repositories/itemRepository.js
@@ -1,16 +1,24 @@
 export const insertItemQuery = `
   INSERT INTO items (itemName, price, dailyIncome, itemImage, duration)
   VALUES ($1, $2, $3, $4, $5)
+  RETURNING *;
 `;
+
 export const getItemByIdQuery = `
-  SELECT * FROM items WHERE id = $1
+  SELECT * FROM items WHERE id = $1;
 `;
+
 export const getAllItemsQuery = `
-  SELECT * FROM items
+  SELECT * FROM items ORDER BY id ASC;
 `;
+
 export const updateItemQuery = `
-  UPDATE items  SET itemName = $2, price = $3, dailyIncome = $4, itemImage = $5, duration = $6 WHERE id = $1
+  UPDATE items 
+  SET itemName = $2, price = $3, dailyIncome = $4, itemImage = $5, duration = $6 
+  WHERE id = $1
+  RETURNING *;
 `;
+
 export const deleteItemQuery = `
-  DELETE FROM items WHERE id = $1
+  DELETE FROM items WHERE id = $1;
 `;
